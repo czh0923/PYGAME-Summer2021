34dc0e34047d6020d591c202a7313a6c61b99f89
Zhihan Cheng <zchenggn@126.com>
2021-08-01 01:55:36 -0700

    load file with json; fixed multiple click bug

--- White-board/main.py
+++ White-board/main.py
@@ -3,6 +3,7 @@ from grid import *
 from toolbar import *
 from draw_color import *
 import os
+import json
 
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Drawing Program")
@@ -10,13 +11,19 @@ print(ROWS, '*', ROWS)
 
 def save_file(grid):
     save_path = 'histories'
-    #file_name = "test.txt"
     file_name = str(input("Save as? "))
     file_dir = os.path.join(save_path, file_name)
 
     f = open(file_dir, "w")
-    f.write(str(grid))
-    f.close()  
+    json.dump(grid, f)
+
+def load_file():
+    save_path = 'histories'
+    file_name = str(input("File name?"))
+    file_dir = os.path.join(save_path, file_name)
+
+    f = open(file_dir, "r")
+    return json.load(f)
 
 
 def main():
@@ -45,25 +52,27 @@ def main():
                     draw(grid, x, y, GRID_SIZE, draw_color)
 
                 except IndexError:
+                    if event.type == pygame.MOUSEBUTTONDOWN:
 
-                    action = handle_button(x, y, COLOR_BUTTON, WHITE, COLORS)
+                        action = handle_button(x, y, COLOR_BUTTON, WHITE, COLORS)
 
-                    if action == 1: # clear
-                        grid = clear_grid(grid, ROWS, COLS, WHITE)
-                        print('Cleared!')
+                        if action == 1: # clear
+                            grid = clear_grid(grid, ROWS, COLS, WHITE)
+                            print('Cleared!')
 
-                    elif action == 2:
-                        save_file(grid)
-                        print('Saved!')
+                        elif action == 2: # save file
+                            save_file(grid)
+                            print('Saved!')
+                            
+                        elif action == 3: # load file
+                            grid = load_file()
+                            print('Loaded!')
 
-                    elif action == 3: #load TODO
-                        pass
+                        elif action is None: # clicked on the margin
+                            pass # do nothing
 
-                    elif action is None: # clicked on the margin
-                        pass # do nothing
-
-                    else:
-                        draw_color = action
+                        else:
+                            draw_color = action
 
         draw_grid(grid, WIN, ROWS, COLS, GRID_SIZE)
 
